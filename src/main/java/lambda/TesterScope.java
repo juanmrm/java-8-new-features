466e9489642d3a2e7e9355da37bb176f833a7007
jmrodriguez <juan-miguel.rodriguez@soprasteria.com>
2018-01-26 10:58:24 +0100

    [DOCS]: Update docs.

--- src/main/java/lambda/TesterScope.java
+++ src/main/java/lambda/TesterScope.java
@@ -1,5 +1,7 @@
 package lambda;
 
+import java.util.function.Consumer;
+
 class TesterScope {
 
     private final static String salutation = "Hello! ";
@@ -7,6 +9,10 @@ class TesterScope {
     public static void main(String args[]) {
         GreetingService greetService1 = message -> System.out.println(salutation + message);
         greetService1.sayMessage("Mahesh");
+
+        //int x= 0; //If we uncomment this line, there is a compilation error because of the scope.
+        Consumer<String> func1 = x -> System.out.println(x);
+        func1.accept("");
     }
 
     interface GreetingService {
